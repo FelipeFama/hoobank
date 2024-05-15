ff6db75d1f2f3971b737347866ad5cbb3f56a8b6
FelipeFama <lipehfama@gmail.com>
2024-05-15 17:19:32 -0300

    change scope in project

--- index.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-  let menuToggle = document.querySelector('.toggle');
-  let navigation = document.querySelector('.navigation');
-  menuToggle.classList.toggle('active');
-  navigation.classList.toggle('active');
-}
