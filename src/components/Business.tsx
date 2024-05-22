542e341b466e473f417da0e053b6fd0f3d0ffba3
FelipeFama <lipehfama@gmail.com>
2024-05-22 14:58:59 -0300

    feat: add new tags in business component

--- src/components/Business.tsx
+++ src/components/Business.tsx
@@ -1,5 +1,33 @@
+import { features } from "../constants";
 import { box, layout } from "../constants/boxes";
 import Button from "./Button";
+
+interface featureCardProps {
+  icon: string;
+  title: string;
+  content: string;
+  index: number;
+}
+
+const FeatureCard = ({ icon, title, content, index }: featureCardProps) => (
+  <aside
+    className={`flex flex-row p-6 rounded-[20px] ${
+      index !== features.length - 1 ? "mb-6" : "mb-0"
+    } feature-card`}>
+    <figure
+      className={`w-[64px] h-[64px] rounded-full ${box.flexCenter} bg-dimBlue`}>
+      <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
+    </figure>
+    <div className="flex-1 flex flex-col ml-3">
+      <h3 className="font-poppins font-semibold text-white text-[18px] leading-[23.4px] mb-1">
+        {title}
+      </h3>
+      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px]">
+        {content}
+      </p>
+    </div>
+  </aside>
+);
 
 export default function Business() {
   return (
@@ -15,6 +43,11 @@ export default function Business() {
           of credit cards on the market.
         </p>
         <Button styles={`mt-10`} />
+      </article>
+      <article className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id} {...feature} index={index} />
+        ))}
       </article>
     </section>
   );
