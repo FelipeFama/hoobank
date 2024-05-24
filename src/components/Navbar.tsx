896a1120facaa6e439615718713db78dab7a656c
FelipeFama <lipehfama@gmail.com>
2024-05-23 21:14:15 -0300

    update navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,7 +14,7 @@ export default function Navbar() {
             {navLinks.map((nav, index) => (
               <li
                 key={nav.id}
-                className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${
+                className={`font-poppins font-normal cursor-pointer text-[16px] text-white hover:text-secondary ${
                   index === navLinks.length - 1 ? "mr-0" : "mr-10"
                 }`}>
                 <a href={`#${nav.id}`}>{nav.title}</a>
@@ -35,7 +35,7 @@ export default function Navbar() {
                 {navLinks.map((nav, index) => (
                   <li
                     key={nav.id}
-                    className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${
+                    className={`font-poppins font-normal cursor-pointer text-[16px] text-white hover:text-secondary ${
                       index === navLinks.length - 1 ? "mr-0" : "mb-4"
                     }`}>
                     <a href={`#${nav.id}`}>{nav.title}</a>
